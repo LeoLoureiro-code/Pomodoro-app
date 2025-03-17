e6f31618f6aac33422f8ab6d4a950757476525df
Leo Loureiro <leolou.90@gmail.com>
2025-03-16 18:47:02 -0700

    add clock and timer components with tags and styles

---
+++ src/components/clock/clock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clock',
+  standalone: true,
+  imports: [],
+  templateUrl: './clock.component.html',
+  styleUrl: './clock.component.scss'
+})
+export class ClockComponent {
+ 
+}
---
+++ src/components/timer/timer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss'
+})
+export class TimerComponent {
+
+}
