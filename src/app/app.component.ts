b9a32d2cd288f02f2a78f6d5e39dddb2baa1ed39
Leo Loureiro <leolou.90@gmail.com>
2025-03-19 18:47:46 -0700

    remove timer component for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TimerComponent } from "../components/timer/timer.component";
 import { ClockComponent } from "../components/clock/clock.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TimerComponent, ClockComponent],
+  imports: [ClockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
