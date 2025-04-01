1c9774f46290f12596124015488561d9068ea5dd
Leo Loureiro <leolou.90@gmail.com>
2025-03-31 18:39:57 -0700

    add methods to open and close modal and modal service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModalService } from '../services/modal-service.service';
+import { Subscription } from 'rxjs';
 import { ClockComponent } from "../components/clock/clock.component";
+import { SettingsComponent } from "../components/settings/settings.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ClockComponent],
+  imports: [ClockComponent, SettingsComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Pomodoro-app';
+  isModalVisible: boolean = false;
+  private modalSubscription!: Subscription; 
+
+  constructor(private modalService: ModalService) {
+    this.modalSubscription = this.modalService.isModalVisible$.subscribe(isVisible => {
+      this.isModalVisible = isVisible;
+    });
+  }
+
+  OpenModal() {
+    this.modalService.ShowModal();
+  }
+
+  CloseModal() {
+    this.modalService.HideModal();
+  }
+
+  ngOnDestroy(): void {
+    if (this.modalSubscription) {
+      this.modalSubscription.unsubscribe(); 
+    }
+  }
 }
