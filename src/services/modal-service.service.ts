1af4a7aed4486ee18242a9a63ce3e55a35581179
Leo Loureiro <leolou.90@gmail.com>
2025-03-31 18:43:44 -0700

    service to show and hide modal

--- src/services/modal-service.service.ts
+++ src/services/modal-service.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ModalServiceService {
+export class ModalService {
+
+  private isModalVisibleSubject = new BehaviorSubject<boolean>(false);
+  
+  isModalVisible$ = this.isModalVisibleSubject.asObservable();
 
   constructor() { }
+
+  ShowModal(){
+    this.isModalVisibleSubject.next(true);
+  }
+
+  HideModal(){
+    this.isModalVisibleSubject.next(false);
+  }
 }
+
+
