5f92af4a7ba4231c5dfc0f1ad13179c71d282155
Leo Loureiro <leolou.90@gmail.com>
2025-04-02 19:01:32 -0700

    add values from timer service

--- src/components/clock/clock.component.ts
+++ src/components/clock/clock.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TimerService } from '../../services/timer.service';
 
 @Component({
   selector: 'clock',
@@ -9,99 +9,125 @@ import { CommonModule } from '@angular/common';
   templateUrl: './clock.component.html',
   styleUrl: './clock.component.scss'
 })
+export class ClockComponent implements OnInit {
+  constructor(private elementRef: ElementRef, private timerService: TimerService) {}
 
-export class ClockComponent implements OnInit{
+  // Timer Durations (User inputs from service)
+  pomodoroTime: number = 25;
+  shortBreakTime: number = 5;
+  longBreakTime: number = 15;
 
+  // Convert minutes to seconds
+  pomodoroTimeInSeconds: number = this.pomodoroTime * 60;
+  shortBreakTimeInSeconds: number = this.shortBreakTime * 60;
+  longBreakTimeInSeconds: number = this.longBreakTime * 60;
 
-  constructor(private elementRef: ElementRef) {}
+  totalSeconds: number = this.pomodoroTimeInSeconds; 
+  timerMinutes!: string;
+  timerSeconds!: string;
+  timerTracker!: any;
+  cycleIndex: number = 0;
+  isRunning: boolean = false;
 
-// Timer Durations. User isputs
-pomodoroTime: number = 0.2; 
-shortBreakTime: number = 0.1;
-longBreakTime: number = 0.3;
+  currentSessionType: 'pomodoro' | 'short-break' | 'long-break' = 'pomodoro';
 
-// Convert minutes to seconds
-pomodoroTimeInSeconds: number = this.pomodoroTime * 60;
-shortBreakTimeInSeconds: number = this.shortBreakTime * 60;
-longBreakTimeInSeconds: number = this.longBreakTime * 60;
+  sessionSequence: { type: string; duration: number }[] = [];
 
-totalSeconds: number = this.pomodoroTimeInSeconds; 
-timerMinutes!: string;
-timerSeconds!: string;
-timerTracker!: any;
-cycleIndex: number = 0; 
-isRunning: boolean = false;
+  // Buttons
+  startBtn!: HTMLElement;
+  pauseBtn!: HTMLElement;
 
-currentSessionType: 'pomodoro' | 'short-break' | 'long-break' = 'pomodoro';
+  ngOnInit() {
+    this.startBtn = this.elementRef.nativeElement.querySelector(".start-btn");
+    this.pauseBtn = this.elementRef.nativeElement.querySelector(".pause-btn");
 
-sessionSequence: { type: string; duration: number }[] = [
-  { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },  
-  { type: 'short-break', duration: this.shortBreakTimeInSeconds }, 
-  { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },  
-  { type: 'short-break', duration: this.shortBreakTimeInSeconds }, 
-  { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },  
-  { type: 'short-break', duration: this.shortBreakTimeInSeconds }, 
-  { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },  
-  { type: 'long-break', duration: this.longBreakTimeInSeconds }, 
-];
+    // Get updated timer values from service
+    this.timerService.getPomodoroTime().subscribe((pomodoroTime) => {
+      this.pomodoroTime = pomodoroTime;
+      this.updateSessionDurations();
+    });
 
-// Buttons
-startBtn!: HTMLElement;
-pauseBtn!: HTMLElement;
+    this.timerService.getShortBreakTime().subscribe((shortTime) => {
+      this.shortBreakTime = shortTime;
+      this.updateSessionDurations();
+    });
 
+    this.timerService.getLongBreakTime().subscribe((longTime) => {
+      this.longBreakTime = longTime;
+      this.updateSessionDurations();
+    });
 
+    this.DisplayTime();
+  }
 
-ngOnInit() {
-  this.DisplayTime();
-  this.startBtn = this.elementRef.nativeElement.querySelector(".start-btn");
-  this.pauseBtn = this.elementRef.nativeElement.querySelector(".pause-btn");
-}
+  updateSessionDurations() {
+    
+    // Convert minutes to seconds
+    this.pomodoroTimeInSeconds = this.pomodoroTime * 60;
+    this.shortBreakTimeInSeconds = this.shortBreakTime * 60;
+    this.longBreakTimeInSeconds = this.longBreakTime * 60;
 
-UpdateCountDown() {
-  this.timerTracker = setInterval(() => {
-    if (this.totalSeconds <= 0) {
-      clearInterval(this.timerTracker);
-      this.MoveToNextSession(); 
-    } else {
-      this.DisplayTime();
-      this.totalSeconds -= 1;
+    // Update session sequence
+    this.sessionSequence = [
+      { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },
+      { type: 'short-break', duration: this.shortBreakTimeInSeconds },
+      { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },
+      { type: 'short-break', duration: this.shortBreakTimeInSeconds },
+      { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },
+      { type: 'short-break', duration: this.shortBreakTimeInSeconds },
+      { type: 'pomodoro', duration: this.pomodoroTimeInSeconds },
+      { type: 'long-break', duration: this.longBreakTimeInSeconds },
+    ];
+
+    // Reset to first session
+    this.cycleIndex = 0;
+    this.totalSeconds = this.sessionSequence[this.cycleIndex].duration;
+    this.currentSessionType = this.sessionSequence[this.cycleIndex].type as any;
+    this.DisplayTime();
+  }
+
+  UpdateCountDown() {
+    this.timerTracker = setInterval(() => {
+      if (this.totalSeconds <= 0) {
+        clearInterval(this.timerTracker);
+        this.MoveToNextSession();
+      } else {
+        this.DisplayTime();
+        this.totalSeconds -= 1;
+      }
+    }, 1000);
+  }
+
+  DisplayTime() {
+    const seconds = this.totalSeconds % 60;
+    const minutes = Math.floor(this.totalSeconds / 60);
+    this.timerMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    this.timerSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  }
+
+  StartTimer() {
+    this.pauseBtn.style.display = "block";
+    this.startBtn.style.display = "none";
+    this.UpdateCountDown();
+  }
+
+  PauseTimer() {
+    clearInterval(this.timerTracker);
+    this.pauseBtn.style.display = "none";
+    this.startBtn.style.display = "block";
+    this.DisplayTime();
+  }
+
+  MoveToNextSession() {
+    this.cycleIndex++;
+
+    if (this.cycleIndex >= this.sessionSequence.length) {
+      this.isRunning = false;
+      return;
     }
-  }, 1000);
-}
 
-DisplayTime() {
-  const seconds = this.totalSeconds % 60;
-  const minutes = Math.floor(this.totalSeconds / 60);
-  this.timerMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-  this.timerSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-}
-
-StartTimer() {
-  this.pauseBtn.style.display = "block";
-  this.startBtn.style.display = "none";
-  this.UpdateCountDown();
-}
-
-PauseTimer() {
-  clearInterval(this.timerTracker);
-  this.pauseBtn.style.display = "none";
-  this.startBtn.style.display = "block";
-  this.DisplayTime();
-}
-
-MoveToNextSession() {
-  this.cycleIndex++;
-
-if (this.cycleIndex >= this.sessionSequence.length) {
-  this.isRunning = false;
-  return; 
-}
-
-this.totalSeconds = this.sessionSequence[this.cycleIndex].duration;
-this.currentSessionType = this.sessionSequence[this.cycleIndex].type as any;
-
-this.UpdateCountDown();
-
-}
-
+    this.totalSeconds = this.sessionSequence[this.cycleIndex].duration;
+    this.currentSessionType = this.sessionSequence[this.cycleIndex].type as any;
+    this.UpdateCountDown();
+  }
 }
