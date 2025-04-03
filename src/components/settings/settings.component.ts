78aa76322b8f955292447f2e350506fa744e2652
Leo Loureiro <leolou.90@gmail.com>
2025-04-02 19:03:19 -0700

    add functions to send values to timer service

--- src/components/settings/settings.component.ts
+++ src/components/settings/settings.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, Output, EventEmitter, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+import { TimerService } from '../../services/timer.service';
+
 @Component({
   selector: 'settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule], 
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
@@ -12,7 +15,26 @@ export class SettingsComponent {
   @Input() isVisible: boolean = false;
   @Output() closeModal = new EventEmitter<void>();
 
-  onClose(){
+  pomodoroInput: number = 25;
+  shortBreakInput: number = 5;
+  longBreakInput: number = 15;
+
+  constructor(private timerService: TimerService) {}
+
+  // Send values to TimerService
+  sendPomodoroTime() {
+    this.timerService.setPomodoroTime(this.pomodoroInput);
+  }
+
+  sendShortBreakTime() {
+    this.timerService.setShortBreakTime(this.shortBreakInput);
+  }
+
+  sendLongBreakTime() {
+    this.timerService.setLongBreakTime(this.longBreakInput);
+  }
+
+  onClose() {
     this.isVisible = false;
     this.closeModal.emit();
   }
